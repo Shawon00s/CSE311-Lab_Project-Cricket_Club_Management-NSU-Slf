caf4547bc70ca33bdfb488921c49685b759713b8
Shawon00s <royshawonsudipto@gmail.com>
2025-01-07 12:11:54 +0600

    updated user-site

---
+++ User-Site/add-player-statistics.php
@@ -0,0 +1,143 @@
+<?php
+include '../connect.php';
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+    $runs_scored = $_POST['runs_scored'];
+    $wickets_taken = $_POST['wickets_taken'];
+    $balls_faced = $_POST['balls_faced'];
+    $fours_hit = $_POST['fours_hit'];
+    $sixes_hit = $_POST['sixes_hit'];
+    $overs_bowled = $_POST['overs_bowled'];
+    $stumpings = $_POST['stumpings'];
+    $run_outs = $_POST['run_outs'];
+    $catches = $_POST['catches'];
+    $player_id = $_POST['player_id'];
+
+    $checkPlayerQuery = "SELECT * FROM player WHERE player_id = '$player_id'";
+    $playerResult = mysqli_query($conn, $checkPlayerQuery);
+
+    if (mysqli_num_rows($playerResult) == 0) {
+        die("Error: The player_id '$player_id' does not exist in the player table.");
+    }
+
+    $sql = "INSERT INTO playerstatistics (runs_scored, wickets_taken, balls_faced, fours_hit, sixes_hit, overs_bowled, stumpings, run_outs, catches, player_id) 
+            VALUES ('$runs_scored', '$wickets_taken', '$balls_faced', '$fours_hit', '$sixes_hit', '$overs_bowled', '$stumpings', '$run_outs', '$catches', '$player_id')";
+
+    if (mysqli_query($conn, $sql)) {
+        header('location:display-player-statistics.php');
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+}
+
+mysqli_close($conn);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/daisyui@4.12.14/dist/full.min.css" rel="stylesheet" type="text/css" />
+    <script src="https://cdn.tailwindcss.com"></script>
+    <title>Add Player Statitics</title>
+</head>
+
+<?php include('partials/header.php'); ?>
+
+<body>
+
+    <div>
+        <h1 class="font-bold text-2xl text-center mt-5">Register Player Statistics</>
+    </div>
+    <form class="max-w-2xl mx-auto pt-6" action="add-player-statistics.php" method="POST">
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_runs"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="runs scored" />
+            <label for="floating_runs"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Runs
+                Scored</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_wicket"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="wickets_taken" />
+            <label for="floating_wicket"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Wickets
+                Taken</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_balls_faced"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="balls_faced" />
+
+            <label for="floating_balls_faced"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Balls
+                faced</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_fours_hit"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="fours_hit" />
+            <label for="floating_fours_hit"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Number
+                of fours</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_sixes_hit"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="sixes hit" />
+            <label for="floating_sixes_hit"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Number
+                of sixes</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_overs"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="overs_bowled" />
+            <label for="floating_overs"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Overs
+                bowled</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_stumpings"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="stumpings" />
+            <label for="floating_stumpings"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Number
+                of stumpings</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_run_outs"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="run_outs" />
+            <label for="floating_run_outs"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Number
+                of runouts</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_catches"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="catches" />
+            <label for="floating_catches"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Number
+                of catches</label>
+        </div>
+        <div class="relative z-0 w-full mb-5 group">
+            <input type="number" id="floating_player_id"
+                class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
+                placeholder=" " required name="player_id" />
+            <label for="player_club_id"
+                class="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Player
+                ID</label>
+        </div>
+
+        <button type="submit"
+            class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
+    </form>
+</body>
+
+</html>
